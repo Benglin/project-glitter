e8e931907aad7bf5c2f9fd7d681e31ff3b2af160
gohb <beng-lin.goh@autodesk.com>
2021-06-17 02:02:36 +0800

    Removed redundant usage of texture

--- assembly/components/Renderer.ts
+++ assembly/components/Renderer.ts
@@ -1,18 +1,13 @@
 import { WebGLRenderingContext } from "../externals/WebGL";
 import { TestTriangle } from "./TestTriangle";
-import { Texture } from "./Texture";
 
 export class Renderer {
     private _triangle: TestTriangle;
     private _gl: WebGLRenderingContext;
 
-    private readonly _t: Texture;
     constructor(canvasId: string, contextType: string) {
         this._gl = new WebGLRenderingContext(canvasId, contextType);
         this._triangle = new TestTriangle(this._gl);
-
-        this._t = new Texture(this._gl);
-        this._t.load("circle.svg");
     }
 
     updateFrame(): void {
