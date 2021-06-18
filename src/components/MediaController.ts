1f6417e90d29f41826a3fd5af4b7caaf9d47d3e2
gohb <beng-lin.goh@autodesk.com>
2021-06-18 18:37:17 +0800

    Exposed 'media-ready' event from 'MediaController'

--- src/components/MediaController.ts
+++ src/components/MediaController.ts
@@ -1,4 +1,6 @@
-export class MediaController {
+export class MediaController extends EventTarget {
+    public static MediaReady = "media-ready";
+
     private _playing: boolean = false;
     private readonly _audioElementId: string;
 
@@ -6,7 +8,24 @@ export class MediaController {
     private _audioContext: AudioContext | null = null;
 
     constructor(audioElementId: string) {
+        super();
+
         this._audioElementId = audioElementId;
+        const e = document.getElementById(this._audioElementId);
+        const audioElement = e as HTMLAudioElement;
+
+        const thisObject = this;
+        audioElement.addEventListener("canplaythrough", (event: Event) => {
+            // Notify all listeners that the media is ready for playback.
+            thisObject.dispatchEvent(new Event(MediaController.MediaReady));
+        });
+    }
+
+    public loadMedia(mediaUrl: string): void {
+        const e = document.getElementById(this._audioElementId);
+        const audioElement = e as HTMLAudioElement;
+        audioElement.src = mediaUrl;
+        audioElement.load();
     }
 
     public connect(): void {
