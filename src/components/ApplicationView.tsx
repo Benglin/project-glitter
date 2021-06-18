fa6c2e895171f86a4bb7a96f45b714843152f9bc
gohb <beng-lin.goh@autodesk.com>
2021-06-18 21:28:44 +0800

    Added page layout and GitHub link

--- src/components/ApplicationView.tsx
+++ src/components/ApplicationView.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useRef, useState } from "react";
 import { Application } from "../Application";
-import { makeStyles } from "@material-ui/core/styles";
-import { Typography } from "@material-ui/core";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import GitHubIcon from "@material-ui/icons/GitHub";
+
+import { makeStyles, createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
 const mainTheme = createMuiTheme({
     palette: {
@@ -12,7 +15,26 @@ const mainTheme = createMuiTheme({
 
 const useStyles = makeStyles((theme) => ({
     root: {
+        display: "flex",
+        flexDirection: "column",
+        height: `calc(100% - ${theme.spacing(4)}px)`,
         padding: `${theme.spacing(2)}px`,
+    },
+    header: {},
+    content: {
+        flexGrow: 1,
+    },
+    footer: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "flex-end",
+        justifyContent: "space-between",
+    },
+    footerLeft: {},
+    footerRight: {},
+    iconButton: {
+        margin: `0px`,
+        padding: `0px`,
     },
 }));
 
@@ -46,9 +68,29 @@ function ApplicationView() {
     return (
         <ThemeProvider theme={mainTheme}>
             <div className={classes.root}>
-                <Typography variant="body2" color="textPrimary">
-                    {mediaReady ? `Fps: ${frameRate.toFixed(2)}` : "Media Not Ready"}
-                </Typography>
+                <div className={classes.header}>
+                    <Typography variant="body2" color="textPrimary">
+                        {mediaReady ? `Fps: ${frameRate.toFixed(2)}` : "Media Not Ready"}
+                    </Typography>
+                </div>
+                <div className={classes.content}></div>
+                <div className={classes.footer}>
+                    <div className={classes.footerLeft}>
+                        <Typography variant="body2" color="textPrimary">
+                            {mediaReady ? "Click to play 'Bajan Canadian (Minecraft Mix)'" : ""}
+                        </Typography>
+                    </div>
+                    <div className={classes.footerRight}>
+                        <IconButton
+                            aria-label="delete"
+                            className={classes.iconButton}
+                            target="_blank"
+                            href="https://github.com/benglin/project-glitter"
+                        >
+                            <GitHubIcon color="secondary" fontSize="large" />
+                        </IconButton>
+                    </div>
+                </div>
             </div>
         </ThemeProvider>
     );
